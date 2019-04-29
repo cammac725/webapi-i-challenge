91548cb262af93c998950128422b4b690f243324
cammac <camm@rocktechcomputer.com>
2019-04-29 13:13:50 -0700

    added get users by id endpoint

--- index.js
+++ index.js
@@ -15,7 +15,28 @@ server.get('/api/users', (req, res) => {
       res.status(200).json({ success: true, users })
     })
     .catch(err => {
-      res.status(err.code).json({ success: false, })
+      res.status(500).json({ error: "The users information could not be retrieved." })
+    })
+})
+
+server.get('/api/users/:id', (req, res) => {
+  const id = req.params.id;
+  db.findById(id)
+    .then(user => {
+      if (user) {
+        res.status(200).json({
+          success: true,
+          user
+        })
+      } else {
+        res.status(404).json({
+          success: false,
+          message: "The user with the specified ID does not exist."
+        })
+      }
+    })
+    .catch(err => {
+      res.status(500).json({ error: "The user information could not be retrieved." })
     })
 })
 
