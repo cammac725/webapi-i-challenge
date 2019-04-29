46ba5db46f4d26a9a6ee8cd9925eae8e32a1ef9d
cammac <camm@rocktechcomputer.com>
2019-04-29 13:03:42 -0700

    Initial, added get users endpoint

--- index.js
+++ index.js
@@ -1,1 +1,24 @@
 // implement your API here
+const express = require('express');
+const db = require('./data/db.js');
+
+const server = express();
+server.use(express.json());
+
+server.get('/', (req, res) => {
+  res.send('Welcome to Lambda Users')
+})
+
+server.get('/api/users', (req, res) => {
+  db.find()
+    .then(users => {
+      res.status(200).json({ success: true, users })
+    })
+    .catch(err => {
+      res.status(err.code).json({ success: false, })
+    })
+})
+
+server.listen(3000, () => {
+  console.log("\nServer is running on port 3000\n")
+})
